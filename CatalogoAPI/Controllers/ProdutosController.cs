55e3aaea718a36c03a013d59d8c5db405837eb7e
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2025-01-23 11:15:04 -0300

    Add put and delete in controllers

--- CatalogoAPI/Controllers/ProdutosController.cs
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogoAPI.Context;
 using CatalogoAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogoAPI.Controllers
 {
@@ -39,9 +40,41 @@ namespace CatalogoAPI.Controllers
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
             _context.Produtos.Add(produto);
             _context.SaveChanges();
             return Created();
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id) 
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
 
     }
